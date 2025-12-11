8cefd5782a909f8bf930dcb458f067f8810af326
Anna sabbagh <afreenshabbagh@gmail.com>
2025-12-11 10:14:25 +0530

    fix: updated CORS config to allow POST preflight and enable frontend requests

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ import nodemailer from "nodemailer";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 
 // -------- SEND EMAIL ROUTE -----------
