b2af96734b6eb0941c421b6384f98b9a5705e408
Anna sabbagh <afreenshabbagh@gmail.com>
2025-12-11 14:59:28 +0530

    add https://as-consultancy.vercel.app this

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://as-consultancy.vercel.app" 
   ],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
